0d88de064fc5b807b3985fd44bf4d70536bd6810
ralvescosta <rafael.rac.mg@gmail.com>
2022-01-13 08:45:38 -0300

    feat: add database

--- blockchain.go
+++ blockchain.go
@@ -1,25 +1,142 @@
 package main
 
+import (
+	"github.com/dgraph-io/badger"
+)
+
 type BlockChain struct {
-	Blocks []*Block
+	LastHash []byte
+	Database *badger.DB
 }
 
+const dbPath = "./tmp/blocks"
+
 func (pst *BlockChain) Add(data string) error {
-	index := len(pst.Blocks) - 1
-	prevBlock := pst.Blocks[index]
+	var lastHash []byte
 
-	err, newBlock := NewBlock(data, prevBlock.Hash, prevBlock.Id+1)
+	err := pst.Database.View(func(txn *badger.Txn) error {
+		item, err := txn.Get([]byte("lh"))
+		if err != nil {
+			return err
+		}
 
+		err = item.Value(func(val []byte) error {
+			lastHash = val
+			return nil
+		})
+		return err
+	})
 	if err != nil {
 		return err
 	}
 
-	pst.Blocks = append(pst.Blocks, newBlock)
-	return nil
+	err, newBlock := NewBlock(data, lastHash, 0)
+
+	err = pst.Database.Update(func(transaction *badger.Txn) error {
+		err, serialized := newBlock.Serialize()
+		if err != nil {
+			return err
+		}
+
+		err = transaction.Set(newBlock.Hash, serialized)
+		if err != nil {
+			return err
+		}
+
+		err = transaction.Set([]byte("lh"), newBlock.Hash)
+		pst.LastHash = newBlock.Hash
+		return err
+	})
+	return err
 }
 
-func NewBlockchain() BlockChain {
-	_, block := NewBlock("Chain Initialized", []byte{}, 0)
+func NewBlockchain() (error, *BlockChain) {
+	var lastHash []byte
+	opts := badger.DefaultOptions(dbPath)
+	db, err := badger.Open(opts)
 
-	return BlockChain{[]*Block{block}}
+	if err != nil {
+		return err, nil
+	}
+
+	err = db.Update(func(txn *badger.Txn) error {
+		if _, err := txn.Get([]byte("lh")); err == badger.ErrKeyNotFound {
+			err, block := NewBlock("Chain Initialized", []byte{}, 0)
+			if err != nil {
+				return err
+			}
+
+			err, serialized := block.Serialize()
+			if err != nil {
+				return err
+			}
+
+			err = txn.Set(block.Hash, serialized)
+			if err != nil {
+				return err
+			}
+
+			err = txn.Set([]byte("lh"), block.Hash)
+			if err != nil {
+				return err
+			}
+
+			lastHash = block.Hash
+
+			return err
+		} else {
+			item, err := txn.Get([]byte("lh"))
+			if err != nil {
+				return err
+			}
+			err = item.Value(func(val []byte) error {
+				lastHash = val
+				return nil
+			})
+
+			return err
+		}
+	})
+
+	return nil, &BlockChain{lastHash, db}
+}
+
+type BlockChainIterator struct {
+	CurrentHash []byte
+	Database    *badger.DB
+}
+
+func (chain *BlockChain) Iterator() *BlockChainIterator {
+	iterator := BlockChainIterator{chain.LastHash, chain.Database}
+
+	return &iterator
+}
+
+func (iterator *BlockChainIterator) Next() (error, *Block) {
+	var block *Block
+
+	err := iterator.Database.View(func(txn *badger.Txn) error {
+		item, err := txn.Get(iterator.CurrentHash)
+		if err != nil {
+			return err
+		}
+
+		err = item.Value(func(val []byte) error {
+			err, block = Deserialize(val)
+			if err != nil {
+				return err
+			}
+
+			return nil
+		})
+
+		return err
+	})
+	if err != nil {
+		return err, nil
+	}
+
+	iterator.CurrentHash = block.PrevHash
+
+	return nil, block
 }
