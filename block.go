097c9b985573d3a6729cff6f7fe5dbb750ef6405
ralvescosta <rafael.rac.mg@gmail.com>
2022-01-13 08:45:13 -0300

    refactor: block

--- block.go
+++ block.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"bytes"
-	"crypto/sha256"
+	"encoding/gob"
 	"fmt"
+	"log"
 	"time"
 )
 
@@ -16,18 +17,42 @@ type Block struct {
 	Nonce     int
 }
 
-func (pst *Block) DeriveHash() {
-	concatenated := bytes.Join([][]byte{pst.Data, pst.PrevHash}, []byte{})
-
-	hash := sha256.Sum256(concatenated)
-
-	pst.Hash = hash[:]
-}
-
 func (pst Block) ToString() string {
 	return fmt.Sprintf("\n[Block] %d\n[Timestamp] %d\n[Data] %x\n[Hash] %x\n[PrevHash] %x\n", pst.Id, pst.Timestamp, pst.Data, pst.Hash, pst.PrevHash)
 }
 
+// Converte the entire block to byte, to make it possible to save
+func (b *Block) Serialize() (error, []byte) {
+	var res bytes.Buffer
+	encoder := gob.NewEncoder(&res)
+
+	err := encoder.Encode(b)
+
+	if err != nil {
+		log.Println("[Err][Block::Serialize]", err)
+		return err, nil
+	}
+
+	return nil, res.Bytes()
+}
+
+// Deserialize the saved block
+func Deserialize(data []byte) (error, *Block) {
+	var block Block
+
+	decoder := gob.NewDecoder(bytes.NewReader(data))
+
+	err := decoder.Decode(&block)
+
+	if err != nil {
+		log.Println("[Err][Block::Deserialize]", err)
+		return err, nil
+	}
+
+	return nil, &block
+}
+
+// Create a new Block
 func NewBlock(data string, prevHash []byte, index uint64) (error, *Block) {
 	block := &Block{
 		Id:        index,
