d42737ac45bdd06e82181a8446d945510eebe8ec
ralvescosta <rafael.rac.mg@gmail.com>
2022-01-14 08:18:04 -0300

    feat: pkg cli

---
+++ pkg/interfaces/cli/cli.go
@@ -0,0 +1,88 @@
+package cli
+
+import (
+	"log"
+	"os"
+	"runtime"
+	"strconv"
+
+	pkgBlock "blockchain/pkg/block"
+	"blockchain/pkg/blockchain"
+)
+
+type CommandLine struct {
+	blockchain *blockchain.BlockChain
+}
+
+//printUsage will display what options are availble to the user
+func (cli *CommandLine) printUsageCommand() {
+	log.Println("Usage: ")
+	log.Println(" add -block <BLOCK_DATA> - add a block to the chain")
+	log.Println(" print - prints the blocks in the chain")
+}
+
+//validateArgs ensures the cli was given valid input
+func (cli *CommandLine) validateArgs() {
+	if len(os.Args) < 2 {
+		cli.printUsageCommand()
+		runtime.Goexit()
+	}
+}
+
+//addBlock allows users to add blocks to the chain via the cli
+func (pst *CommandLine) addBlockCommand(data string) {
+	err := pst.blockchain.Add(data)
+	if err != nil {
+		log.Panic(err)
+	}
+	log.Println("Added Block!")
+}
+
+//printChain will display the entire contents of the blockchain
+func (pst *CommandLine) printChainCommand() {
+	iterator := pst.blockchain.Iterator()
+
+	for {
+		block, err := iterator.Next()
+		if err != nil {
+			log.Panic(err)
+		}
+
+		log.Printf("Previous hash: %x\n", block.PrevHash)
+		log.Printf("data: %s\n", block.Data)
+		log.Printf("hash: %x\n", block.Hash)
+		pow := pkgBlock.NewProofOfWork(block)
+		_, isValid := pow.Validate()
+		log.Printf("Pow: %s\n", strconv.FormatBool(isValid))
+		log.Println()
+		// This works because the Genesis block has no PrevHash to point to.
+		if len(block.PrevHash) == 0 {
+			break
+		}
+	}
+}
+
+func (pst *CommandLine) stateMachine(args []string) {
+	switch args[3] {
+	case "add":
+		addBlockData := args[5]
+		if addBlockData == "" {
+			pst.printUsageCommand()
+			runtime.Goexit()
+		}
+		pst.addBlockCommand(addBlockData)
+	case "print":
+		pst.printChainCommand()
+	default:
+		pst.printUsageCommand()
+	}
+}
+
+func (pst *CommandLine) Run() {
+	pst.validateArgs()
+	pst.stateMachine(os.Args)
+}
+
+func NewCommandLine(blockchain *blockchain.BlockChain) *CommandLine {
+	return &CommandLine{blockchain}
+}
