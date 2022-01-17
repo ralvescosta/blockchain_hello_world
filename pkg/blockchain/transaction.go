12ffde1a67739310599e8257caad81b6890744f4
ralvescosta <rafael.rac.mg@gmail.com>
2022-01-17 06:09:24 -0300

    feat: transaction

---
+++ pkg/blockchain/transaction.go
@@ -0,0 +1,69 @@
+package blockchain
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/gob"
+	"fmt"
+)
+
+const reward = 100
+
+type TxOutput struct {
+	Value  int
+	PubKey string
+}
+
+type TxInput struct {
+	ID  []byte
+	Out int
+	Sig string
+}
+
+type Transaction struct {
+	ID      []byte
+	Inputs  []TxInput  // receiver
+	Outputs []TxOutput // cripto owner
+}
+
+func (tx *Transaction) SetID() error {
+	var encoded bytes.Buffer
+	var hash [32]byte
+
+	encoder := gob.NewEncoder(&encoded)
+	err := encoder.Encode(tx)
+	if err != nil {
+		return err
+	}
+
+	hash = sha256.Sum256(encoded.Bytes())
+	tx.ID = hash[:]
+
+	return nil
+}
+
+func (in *TxInput) CanUnlock(pubKey string) bool {
+	return in.Sig == pubKey
+}
+func (out *TxOutput) CanBeUnlocked(sig string) bool {
+	return out.PubKey == sig
+}
+
+func (tx *Transaction) IsCoinbase() bool {
+	return len(tx.Inputs) == 1 && len(tx.Inputs[0].ID) == 0 && tx.Inputs[0].Out == -1
+}
+
+// First Transaction. The first transaction into a blockchain is called "coinbase"
+func CoinbaseTxn(toAddress, data string) *Transaction {
+	if data == "" {
+		data = fmt.Sprintf("Coins to %s", toAddress)
+	}
+
+	txIn := TxInput{[]byte{}, -1, data}
+
+	txOut := TxOutput{reward, toAddress}
+
+	tx := Transaction{nil, []TxInput{txIn}, []TxOutput{txOut}}
+
+	return &tx
+}
