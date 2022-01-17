00522179a8f0f68a0a6910dfd31244ebaafc1500
ralvescosta <rafael.rac.mg@gmail.com>
2022-01-17 07:02:22 -0300

    feat: transaction

--- pkg/block/block.go
+++ pkg/block/block.go
@@ -1,17 +1,21 @@
 package block
 
 import (
+	"bytes"
+	"crypto/sha256"
 	"fmt"
 	"time"
+
+	txn "blockchain/pkg/transaction"
 )
 
 type Block struct {
-	Id        int64
-	Timestamp int64
-	Hash      []byte
-	Data      []byte
-	PrevHash  []byte
-	Nonce     int
+	Id           int64
+	Timestamp    int64
+	Hash         []byte
+	Transactions []*txn.Transaction
+	PrevHash     []byte
+	Nonce        int
 }
 
 func (pst Block) NextId() int64 {
@@ -19,17 +23,29 @@ func (pst Block) NextId() int64 {
 }
 
 func (pst Block) ToString() string {
-	return fmt.Sprintf("\n[Block] %d\n[Timestamp] %d\n[Data] %x\n[Hash] %x\n[PrevHash] %x\n", pst.Id, pst.Timestamp, pst.Data, pst.Hash, pst.PrevHash)
+	return fmt.Sprintf("\n[Block] %d\n[Timestamp] %d\n[Transactions] %s\n[Hash] %x\n[PrevHash] %x\n", pst.Id, pst.Timestamp, pst.Transactions, pst.Hash, pst.PrevHash)
 }
 
-func NewBlock(data string, prevHash []byte, index int64) (*Block, error) {
+func (b *Block) HashTransactions() []byte {
+	var txHashes [][]byte
+	var txHash [32]byte
+
+	for _, tx := range b.Transactions {
+		txHashes = append(txHashes, tx.ID)
+	}
+	txHash = sha256.Sum256(bytes.Join(txHashes, []byte{}))
+
+	return txHash[:]
+}
+
+func NewBlock(txs []*txn.Transaction, prevHash []byte, index int64) (*Block, error) {
 	block := &Block{
-		Id:        index,
-		Timestamp: time.Now().UnixMilli(),
-		Data:      []byte(data),
-		Hash:      []byte{},
-		PrevHash:  prevHash,
-		Nonce:     0,
+		Id:           index,
+		Timestamp:    time.Now().UnixMilli(),
+		Transactions: txs,
+		Hash:         []byte{},
+		PrevHash:     prevHash,
+		Nonce:        0,
 	}
 
 	pow := NewProofOfWork(block)
