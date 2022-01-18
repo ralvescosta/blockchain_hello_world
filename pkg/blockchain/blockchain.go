8c0fe6c29f8535bd05a51884b8bd1a982fbd127f
ralvescosta <rafael.rac.mg@gmail.com>
2022-01-18 06:28:30 -0300

    refactor: chain capabilites to handle with transactions

--- pkg/blockchain/blockchain.go
+++ pkg/blockchain/blockchain.go
@@ -3,6 +3,8 @@ package blockchain
 import (
 	pkgBlock "blockchain/pkg/block"
 	"blockchain/pkg/repositories"
+	txn "blockchain/pkg/transaction"
+	"encoding/hex"
 )
 
 type BlockChain struct {
@@ -10,13 +12,13 @@ type BlockChain struct {
 	repo     *repositories.Repository
 }
 
-func (pst *BlockChain) Add(data string) error {
+func (pst *BlockChain) Add(transactions []*txn.Transaction) error {
 	block, err := pst.repo.GetLastBlock()
 	if err != nil {
 		return err
 	}
 
-	newBlock, err := pkgBlock.NewBlock(data, block.Hash, block.NextId())
+	newBlock, err := pkgBlock.NewBlock(transactions, block.Hash, block.NextId())
 	if err != nil {
 		return err
 	}
@@ -30,8 +32,90 @@ func (pst *BlockChain) Add(data string) error {
 	return err
 }
 
+func (pst *BlockChain) FindUnspentTransactions(address string) []txn.Transaction {
+	var unspentTxs []txn.Transaction
+
+	spentTXNs := make(map[string][]int)
+
+	iter := pst.Iterator()
+
+	for {
+		block, err := iter.Next()
+		if err != nil {
+			break
+		}
+
+		for _, tx := range block.Transactions {
+			txID := hex.EncodeToString(tx.ID)
+
+		Outputs:
+			for outIdx, out := range tx.Outputs {
+				if spentTXNs[txID] != nil {
+					for _, spentOut := range spentTXNs[txID] {
+						if spentOut == outIdx {
+							continue Outputs
+						}
+					}
+				}
+				if out.CanBeUnlocked(address) {
+					unspentTxs = append(unspentTxs, *tx)
+				}
+			}
+			if !tx.IsCoinbase() {
+				for _, in := range tx.Inputs {
+					if in.CanUnlock(address) {
+						inTxID := hex.EncodeToString(in.ID)
+						spentTXNs[inTxID] = append(spentTXNs[inTxID], in.Out)
+					}
+				}
+			}
+			if len(block.PrevHash) == 0 {
+				break
+			}
+		}
+	}
+	return unspentTxs
+
+}
+
+func (pst *BlockChain) FindUnspentTrancationOutputs(address string) []txn.TxOutput {
+	var unspentTrancationOutput []txn.TxOutput
+	unspentTransactions := pst.FindUnspentTransactions(address)
+	for _, tx := range unspentTransactions {
+		for _, out := range tx.Outputs {
+			if out.CanBeUnlocked(address) {
+				unspentTrancationOutput = append(unspentTrancationOutput, out)
+			}
+		}
+	}
+
+	return unspentTrancationOutput
+}
+
+func (pst *BlockChain) FindSpendableTransactionOutputs(address string, amount int) (int, map[string][]int) {
+	unspentOuts := make(map[string][]int)
+	unspentTxs := pst.FindUnspentTransactions(address)
+	accumulated := 0
+
+Work:
+	for _, tx := range unspentTxs {
+		txID := hex.EncodeToString(tx.ID)
+		for outIdx, out := range tx.Outputs {
+			if out.CanBeUnlocked(address) && accumulated < amount {
+				accumulated += out.Value
+				unspentOuts[txID] = append(unspentOuts[txID], outIdx)
+
+				if accumulated >= amount {
+					break Work
+				}
+			}
+		}
+	}
+	return accumulated, unspentOuts
+}
+
 func NewBlockchain(repo *repositories.Repository) (*BlockChain, error) {
-	firstBlock, err := pkgBlock.NewBlock("First", []byte{}, 0)
+	firstBlock, err := pkgBlock.NewBlock([]*txn.Transaction{}, []byte{}, 0)
 	if err != nil {
 		return nil, err
 	}
